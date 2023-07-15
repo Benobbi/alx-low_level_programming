19820ee6f24888c1671b375885f5f13be1a8df56
Benobbi <bennettcobbiah@gmail.com>
2023-07-14 20:17:21 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * Description: outputs the letters of the alphabet
+ *
+ * Return: always 0
+ */
+
+int main (void)
+{
+	char b;
+	b = 'a';
+	while (b <= 'z')
+	{
+		putchar (b);
+		b++;
+	}
+		putchar('\n');
+
+		return (0);
+}
