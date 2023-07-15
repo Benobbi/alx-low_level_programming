734be642eeebdb8ae12a2b7c5cb430fe72ba7d5b
Benobbi <bennettcobbiah@gmail.com>
2023-07-14 17:58:26 -0700

    task 0, check if number is greater, equal, or less than zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* program runs random numbers and evaluates whether the number is less than zero, equal to zero, or greater than zero
-  */
-
+/* program runs random numbers and evaluates whether the number
+ * is less than zero, equal to zero, or greater than zero
+ */
 /* main - the starting point
  *
  * return: always 0
@@ -12,19 +11,13 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n > 0){
-		printf("%d is positive\n",n);
-	}
-	if (n == 0){
-		printf("%d is zero\n",n);
-	}
-	if (n < 0){
-		printf("%d is negative\n",n);
-	}
-
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
