8dd6286d9bff62ba8233ac4e964da8f7bd5a06c1
Benobbi <bennettcobbiah@gmail.com>
2023-07-14 18:34:28 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,13 +6,13 @@
  * main - the starting point
  * Description: prints an output using the printf function
  *
- * return: always 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -22,5 +22,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	if (n < 0)
 		printf("%d is negative\n", n);
+
 	return (0);
 }
