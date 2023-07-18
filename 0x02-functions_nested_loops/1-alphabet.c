7f5f0d8a12cb035864898e745ba1fba9831572ad
Benobbi <bennettcobbiah@gmail.com>
2023-07-18 15:57:49 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowerase
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar('\n');
+	}
+		return (0);
+}
