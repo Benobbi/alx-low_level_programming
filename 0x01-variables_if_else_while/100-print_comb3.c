107eb28f3c0cc0202547b7e883e7bc06a4b2b495
Benobbi <bennettcobbiah@gmail.com>
2023-07-17 22:33:58 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - output two numbers than do not repeat
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b = a + 1; b <= 9; b++)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		if (a == 8 && b == 9)
+		break;
+		putchar(',');
+		putchar(' ');
+	}
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
