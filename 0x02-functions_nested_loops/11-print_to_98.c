5a7a523e09782c129991aa6602b09ea622936e93
Benobbi <bennettcobbiah@gmail.com>
2023-07-19 00:14:04 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,18 +18,19 @@ void print_to_98(int n)
 		for (a = n; a <= 98; a++)
 		{
 			if (a != 98)
-				printf("%d,", a);
+				printf("%d, ", a);
 			else if (a == 98)
 				printf("%d\n", a);
 		}
-	} else if (n >= 98)
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
 		{
-			for (b = n; b >= 98; b--)
-			{
-				if (b != 98)
-					printf("%d,", b);
-				else if (b == 98)
-					printf("%d\n", b);
+			if (b != 98)
+				printf("%d,", b);
+			else if (b == 98)
+				printf("%d\n", b);
 			}
 		}
 }
