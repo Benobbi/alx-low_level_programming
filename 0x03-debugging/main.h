686aae61f5cafb2eb1c274ddd37ff1dae6a6f4b6
root <root@Bennett>
2023-07-21 02:02:04 +0000

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,5 @@ void positive_or_negative(int i);
 #include <stdio.h>
 int largest_number(int a, int b, int c);
 int convert_remaining_days(int month, int day, int year);
-#endif
+#endif /* MAIN_H */
 	
