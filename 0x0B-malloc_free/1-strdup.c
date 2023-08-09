f48907eccddafde89f85867d4912502a51a08e5b
root <root@Bennett>
2023-08-09 00:03:15 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - create a copy of a memory location
+ * @str: character
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i;
+	int n = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	a = malloc(sizeof(char) * (i + 1));
+	if (a == NULL)
+		return (NULL);
+	for (n = 0; str[n]; n++)
+		a[n] = str[n];
+	return (a);
+}
