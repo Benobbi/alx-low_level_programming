5e83a7d8db1ae82c617921132d9735ec79e7377d
root <root@Bennett>
2023-10-16 22:19:04 +0000

    task 2

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,6 +3,12 @@
 
 #include <stdlib.h>
 
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+div_t div(int a, int b);
+int mod(int a, int b);
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
